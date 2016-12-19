4ce99af1cd96c083f9f7ab8febe47afcedea8786
zqythoem <15810038766@163.com>
2016-12-19 08:58:02 +0800

    recording functions

--- assets/Scrips/Record.js
+++ assets/Scrips/Record.js
@@ -1,86 +1,123 @@
-if(!cc.runtime){
-cc.Class({
-    extends: cc.Component,
+if (!cc.runtime) {
+    cc.Class({
+        extends: cc.Component,
 
-    properties: {
-        recorder:{
-            default:null,
-            type:cc.Node
+        properties: {
+            recorder: {
+                default: null,
+                type: cc.Node
+            },
+            tester: {
+                default: null,
+                type: cc.Label
+            },
         },
-        tester:{
-            default:null,
-            type:cc.Label
-        },
-    },
 
-    // use this for initialization
-    onLoad: function () {
-        var session=new IFlyIatSession({
-        "callback":{
-            "onResult":function(err,result){
-                if(err==null||err==undefined||err==0){
-                    if(result==''||result==null)
-                        this.tester.string="没有获取到识别结果";
-                    else
-                        this.tester.string=result.toString();
-                }else{
-                    this.tester.string=err.toString()+result.toString();
-                }
-            },
-            "onVolume":function(volume){
+        session: null,
+        oldText: "",
+        _result: "",
+
+
+        // use this for initialization
+        onLoad: function() {
+            var _this = this;
+            var oldText = "Listen";
+            var volumeEvent = (function() {
+                var lastVolume = 0;
+                var eventId = 0;
+
+                var listen = function(volume) {
+                    lastVolume = volume;
+                };
                 
-            },
-            "onError":function(){
-                
-            },
-            "onProcess":function(status){
-                switch(status){
-                    case 'onStart':
-                        this.tester.string="服务初始化";
-                        break;
-                    case 'normalVolume':
-                    case 'started':
-                        this.tester.string="倾听中";
-                        break;
-                    case 'onStop':
-                        this.tester.string="等待结果";
-                        break;
-                    case 'onEnd':
-                        this.tester.string=oldText;
-                        break;
-                    case 'lowVolume':
-                        this.tester.string="倾听中(声音过小)";
-                        break;
-                    default:
-                        this.tester.string=status;
+                var start = function() {
+                };
+                var stop = function() {
+                    clearInterval(eventId);
+                };
+                return {
+                    "listen": listen,
+                    "start": start,
+                    "stop": stop
+                };
+            })();
+
+            this.session = new IFlyIatSession({
+                "callback": {
+                    "onResult": function(err, result) {
+                        if (err == null || err == undefined || err == 0) {
+                            if (result == '' || result == null)
+                                //alert("没有获取到识别结果");
+                                _this.tester.string = "没有获取到识别结果"
+                            else{
+                                //alert(result.toString());
+                                _this.tester.string = result.toString();
+                                this._show(result);
+                            }
+                        } else {
+                            alert(err.toString() + result.toString());
+                        }
+                    },
+                    "onVolume": function(volume) {
+
+                    },
+                    "onError": function() {
+
+                    },
+                    "onProcess": function(status) {
+                        switch (status) {
+                            case 'onStart':
+                                //alert("服务初始化");
+                                _this.tester.string = "服务初始化";
+                                break;
+                            case 'normalVolume':
+                            case 'started':
+                                _this.tester.string = "倾听中";
+                                break;
+                            case 'onStop':
+                                _this.tester.string = "等待结果";
+                                break;
+                            case 'onEnd':
+                                _this.tester.string = oldText;
+                                break;
+                            case 'lowVolume':
+                                _this.tester.string = "倾听中(声音过小)";
+                                break;
+                            default:
+                                _this.tester.string = status;
+                        }
+                    }
                 }
+            });
+
+            if (this.recorder) {
+                this.recorder.on(cc.Node.EventType.TOUCH_START, event => {
+                    this._record();
+                }, this);
             }
-        }
-        });
+        },
 
-        if(this.recorder){
-            this.recorder.on(cc.Node.EventType.TOUCH_START,event=>{
-                this._record();
-            },this);
-        }
-    },
-    
-    _record:function(){
-        var ssb_param={
-            "grammar_list": null,
-            "params": "appid=583a97de,appidkey=5417118c65059796, lang = sms, acous = anhui, aue=speex-wb;-1, usr = mkchen, ssm = 1, sub = iat, net_type = wifi, rse = utf8, ent =sms16k, rst = plain, auf  = audio/L16;rate=16000, vad_enable = 1, vad_timeout = 5000, vad_speech_tail = 500, compress = igzip"
-        };
+        _record: function() {
+            var ssb_param = {
+                "grammar_list": null,
+                "params": "appid=583a97de,appidkey=5417118c65059796, lang = sms, acous = anhui, aue=speex-wb;-1, usr = mkchen, ssm = 1, sub = iat, net_type = wifi, rse = utf8, ent =sms16k, rst = plain, auf  = audio/L16;rate=16000, vad_enable = 1, vad_timeout = 5000, vad_speech_tail = 500, compress = igzip"
+            };
+
+            //alert("按下";
+            //alert(ssb_param.toString();
+            this.session.start(ssb_param);
+        },
         
-        //this.tester.string="按下";
-        //this.tester.string=ssb_param.toString();
-        session.start(ssb_param);
-    },
+        _show: function(result) {
+            this._result = result.toString();
+            this.tester.string = this._result;
+        },
 
 
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+        // called every frame, uncomment this function to activate update callback
+        // update: function (dt) {
 
-    // },
-});
+        // },
+    });
 }
